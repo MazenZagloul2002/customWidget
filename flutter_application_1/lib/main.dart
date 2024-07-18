2ea8b705d605cb2d44cc08c5b82510211621478e
MazenZagloul2002 <mazen.zaghloul.1966@gmail.com>
2024-07-18 13:16:38 +0300

    first commit

---
+++ flutter_application_1/lib/main.dart
@@ -0,0 +1,80 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp()); //تاخد widget وتشغلها
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+      useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() { // rebuild
+      _counter++;
+    });
+  }
+  void _decrementCounter() {
+    setState(() { // rebuild
+      _counter--;
+    });
+  }
+  void _resetCounter() {
+    setState(() {
+      _counter = 0;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary, 
+        title: Text(widget.title),),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            ElevatedButton(onPressed:_incrementCounter,child: Text('increment')),
+            ElevatedButton(onPressed:_decrementCounter, child: Text('decrement')) 
+            ,
+            Text(
+              'counter is $_counter',
+              style: Theme.of(context).textTheme.headlineMedium,
+          
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _resetCounter,
+        tooltip: 'reset',
+        child: const Icon(Icons.restart_alt),
+      ), 
+    );
+  }
+}
