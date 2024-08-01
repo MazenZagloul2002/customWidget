8cdefece5f1e6e575aeb3ef118497912900062bc
MazenZagloul2002 <mazen.zaghloul.1966@gmail.com>
2024-08-01 11:46:50 +0300

    CustomWidget

--- flutter_application_1/lib/mazen2.dart
+++ flutter_application_1/lib/mazen2.dart
@@ -8,50 +8,125 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Profile'),
-        ),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Image.network(
-                'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQof2BgqkTGpr7AuecgF7_GFQXVntGioA61Zw&s',
-                width: 100,
-                height: 100,
+      home: MainPage(),
+    );
+  }
+}
+
+class MainPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold
+    (
+      appBar: AppBar(
+        title: Text('Profile'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            Image.network(
+              '',
+              width: 100,
+              height: 100,
+            ),
+            SizedBox(height: 30),
+            Container(
+              color: Colors.black,
+              padding: EdgeInsets.all(20),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'Mazen Profile',
+                    style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+                  ),
+                  SizedBox(height: 10),
+                  Text(
+                    'college: BIS',
+                    style: TextStyle(color: Colors.white),
+                  ),
+                  SizedBox(height: 5),
+                  Text(
+                    'age: 21',
+                    style: TextStyle(color: Colors.white),
+                  ),
+                  SizedBox(height: 5),
+                  Text(
+                    'description:',
+                    style: TextStyle(color: Colors.white),
+                  ),
+                ],
               ),
-              SizedBox(height: 20),
-              Container(
-                color: Colors.black,
-                padding: EdgeInsets.all(20),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      'Mazen Zaghloul Profile',
-                      style: TextStyle(color: Colors.white),
-                    ),
-                    SizedBox(height: 10),
-                    Text(
-                      'college: BIS',
-                      style: TextStyle(color: Colors.white),
-                    ),
-                    Text(
-                      'age: 22',
-                      style: TextStyle(color: Colors.white),
-                    ),
-                    Text(
-                      'description: ',
-                      style: TextStyle(color: Colors.white),
-                    ),
-                  ],
-                ),
-              ),
-            ],
-          ),
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => SecondPage()),
+                );
+              },
+              child: Text('Go to Second Page'),
+            ),
+          ],
         ),
       ),
     );
+  }
+}
+
+class SecondPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold
+    (
+      appBar: AppBar(
+        title: Text('Profile'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.network(
+              '',
+              width: 100,
+              height: 100,
+            ),
+            SizedBox(height: 20),
+            Container(
+              color: Colors.black,
+              padding: EdgeInsets.all(20),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    ' Profile',
+                    style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+                  ),
+                  SizedBox(height: 10),
+                  Text(
+                    'college: name your college',
+                    style: TextStyle(color: Colors.white),
+                  ),
+                  SizedBox(height: 5),
+                  Text(
+                    'age: put your age',
+                    style: TextStyle(color: Colors.white),
+                  ),
+                  SizedBox(height: 5),
+                  Text(
+                    'description: put a description',
+                    style: TextStyle(color: Colors.white),
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(height: 20),
+          ],
+        ),
+      ),
+    );
+    
   }
 }
